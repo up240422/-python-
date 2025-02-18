6e0896ceb0844d06285da13bfd32a750e879675f
up240422 <up240422@alumnos.upa.edu.mx>
2025-02-18 09:26:08 -0600

    dia3

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # Metodologia de la progtramacion 
-## universidad politecnica de aguascalientes 
 
-mip novia nop me quele ayudar en la creacion de mip repositorio 
+
+
+
+
 
 
---
+++ cursoPython/dia3.py
@@ -0,0 +1,16 @@
+# ejercico one
+
+myAge = 18 
+
+# exercie two 
+
+myHeight = 1.75
+
+# exercie three 
+
+z = 4 + 3j 
+
+# exercie four 
+
+base = float(input("Ingresa El Valor: "))
+altura = float(input("Ingresa El Valor: "))
