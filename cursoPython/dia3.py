08f700fb9c7dff5774fc9588d3ee852a36b53a82
up240422 <up240422@alumnos.upa.edu.mx>
2025-02-20 11:49:37 -0600

    dia 3

--- cursoPython/dia3.py
+++ cursoPython/dia3.py
@@ -14,3 +14,28 @@ z = 4 + 3j
 
 base = float(input("Ingresa El Valor: "))
 altura = float(input("Ingresa El Valor: "))
+area = 0.5 * base * altura
+
+#  exercie five 
+
+ladoA = float(input("ingresa el lado A:"))
+ladoB = float(input("ingresa el lado B:"))
+ladoC = float(input("ingresa el lado C:"))
+perimiter = ladoA + ladoB + ladoC 
+print("Tha perimiter is ", perimiter)
+
+# exercie six 
+
+longitud = float(input("ingresa la longitud:"))
+ancho = float(input("ingresa la anchura:"))
+area1 = longitud * ancho 
+perimetro = 2 * (longitud + ancho)
+print("la area es: ",area1)
+print("El perimetro es: ", perimetro)
+
+# exercicie seven 
+
+radio = float(input("Ingresa el radio:"))
+pi = 3.14
+area2 = pi * radio**2  
+circumference = 88
