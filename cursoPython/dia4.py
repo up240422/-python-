d3a67241529c9f1ebd694d5104b37b13f46aff63
up240422 <up240422@alumnos.upa.edu.mx>
2025-02-27 11:51:12 -0600

    dia 4

---
+++ cursoPython/dia4.py
@@ -0,0 +1,38 @@
+# 1. Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'
+print("Ejercicio 1")
+word1 = "thirty"
+word2 = " days"
+word3 = " of"
+word4 = " python"
+cadena = word1 + word2 + word3 + word4
+print(cadena)
+
+# 2. Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+print("")
+print("Ejercicio 2")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
