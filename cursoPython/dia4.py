815623be80736d2781f1905da94a1826fd11296b
up240422 <up240422@alumnos.upa.edu.mx>
2025-03-04 09:34:14 -0600

    dia 4

--- cursoPython/dia4.py
+++ cursoPython/dia4.py
@@ -10,6 +10,55 @@ print(cadena)
 # 2. Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
 print("")
 print("Ejercicio 2")
+word11 = "Coding"
+word22 = " for"
+word33 = " all"
+cadena1 = word11 + word22 + word33 
+print (cadena1)
+
+# 3. Declare a variable named company and assign it to an initial value "Coding For All"
+print("")
+print("Ejercicio 3")
+company = cadena1 
+
+# 4. Print the variable company using print()
+print("")
+print("Ejercicio 4")
+print (company)
+
+# 5. Print the length of the company string using len() method and print()
+print("")
+print("Ejercico 5")
+print(len(company))
+
+# 6. Change all the characters to uppercase letters using upper() method
+print("")
+print("Ejercicio 6")
+company = company.upper()
+print(company)
+
+# 7. Change all the characters to lowercase letters using lower() method.
+print("")
+print("Ejercicio 7")
+company = company.lower()
+print(company)
+
+# 8. Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
+print("")
+print("Ejercicio 8")
+
+
+
+
+
+
+#9. Cut(slice) out the first word of Coding For All string.
+#10. Check if Coding For All string contains a word Coding using the method index, find or other methods.
+#11. Replace the word coding in the string 'Coding For All' to Python.
+#12. Change Python for Everyone to Python for All using the replace method or other methods.
+#13. Split the string 'Coding For All' using space as the separator (split()) .
+#14. "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+
 
 
 
