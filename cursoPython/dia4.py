925638375e53f21354b4fd5d53bad3080a50cf8d
up240422 <up240422@alumnos.upa.edu.mx>
2025-03-05 10:32:42 -0600

    dia 4

--- cursoPython/dia4.py
+++ cursoPython/dia4.py
@@ -46,18 +46,80 @@ print(company)
 # 8. Use capitalize(), title(), swapcase() methods to format the value of the string Coding For All.
 print("")
 print("Ejercicio 8")
+company = company.capitalize()
+print(company)
+company = company.title()
+print(company)
+company = company.swapcase()
+print(company)
+
+# 9. Cut(slice) out the first word of Coding For All string.
+print("")
+print("Ejercicio 9")
+company = "coding for all"
+company = company.strip('coding')
+print(company)
+
+# 10. Check if Coding For All string contains a word Coding using the method index, find or other methods.
 
 
 
 
+# 11. Replace the word coding in the string 'Coding For All' to Python.
+print("")
+print("Ejercicio 11")
+company = company.replace('coding', 'python')
+print(company)
+
+# 12. Change Python for Everyone to Python for All using the replace method or other methods.
+print("")
+print("Ejercicio 12")
+company = "Python for everyone"
+company = company.replace('everyone', 'all')
+print(company)
+
+# 13. Split the string 'Coding For All' using space as the separator (split()) .
+print("")
+print("Ejercicio 13")
+company = "coding for all"  
+company = company.split()
+print(company)
+
+# 14. "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+print("")
+print("Ejercicio 14")
+company = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon"
+print(company.split(','))
+
+# 15. What is the character at index 0 in the string Coding For All.
+print("")
+print("Ejercicio 15")
+company = "coding for all"
+company = company[0]
+print(company)
+
+# 16. What is the last index of the string Coding For All.
+print("")
+print("Ejercicio 16")
+company = "coding for all"
+company = company[-1]
+print(company)
+
+# 17. What character is at index 10 in "Coding For All" string.
 
 
-#9. Cut(slice) out the first word of Coding For All string.
-#10. Check if Coding For All string contains a word Coding using the method index, find or other methods.
-#11. Replace the word coding in the string 'Coding For All' to Python.
-#12. Change Python for Everyone to Python for All using the replace method or other methods.
-#13. Split the string 'Coding For All' using space as the separator (split()) .
-#14. "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" split the string at the comma.
+
+
+# 18. Create an acronym or an abbreviation for the name 'Python For Everyone'.
+# 19. Create an acronym or an abbreviation for the name 'Coding For All'.
+# 20. Use index to determine the position of the first occurrence of C in Coding For All.
+# 21. Use index to determine the position of the first occurrence of F in Coding For All.
+#Use rfind to determine the position of the last occurrence of l in Coding For All People.
+#Use index or find to find the position of the first occurrence of the word 'because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+#Use rindex to find the position of the last occurrence of the word because in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+#Slice out the phrase 'because because because' in the following sentence: 'You cannot end a sentence with because because because is a conjunction'
+
+
 
 
 
